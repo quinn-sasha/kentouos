855abb1b0a971206913df0e73facce3712927e7e
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-08-04 21:32:47 +0900

    Added: memory map

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -1,10 +1,125 @@
 #include  <Uefi.h>
 #include  <Library/UefiLib.h>
+#include  <Library/UefiBootServicesTableLib.h>
+#include  <Library/PrintLib.h>
+#include  <Protocol/LoadedImage.h>
+#include  <Protocol/SimpleFileSystem.h>
+#include  <Protocol/DiskIo2.h>
+#include  <Protocol/BlockIo.h>
+
+struct MemoryMap {
+  UINTN buffer_size;
+  UINTN map_size;
+  void *buffer;
+  UINTN map_key;
+  UINTN discriptor_size;
+  UINTN32 discriptor_version;
+};
+
+const CHAR16* GetMemoryTypeUnicode(EFI_MEMORY_TYPE type) {
+  switch (type) {
+    case EfiReservedMemoryType: return L"EfiReservedMemoryType":
+    case EfiLoaderCode: return L"EfiLoaderCode":
+    case EfiLoaderData: return L"EfiLoaderData":
+    case EfiLoaderData: return L"EfiLoaderData":
+    case EfiBootServicesCode: return L"EfiBootServicesCode";
+    case EfiBootServicesData: return L"EfiBootServicesData";
+    case EfiRuntimeServicesCode: return L"EfiRuntimeServicesCode";
+    case EfiRuntimeServicesData: return L"EfiRuntimeServicesData";
+    case EfiConventionalMemory: return L"EfiConventionalMemory";
+    case EfiUnusableMemory: return L"EfiUnusableMemory";
+    case EfiACPIReclaimMemory: return L"EfiACPIReclaimMemory";
+    case EfiRuntimeServicesData: return L"EfiRuntimeServicesData";
+    case EfiConventionalMemory: return L"EfiConventionalMemory";
+    case EfiUnusableMemory: return L"EfiUnusableMemory";
+    case EfiACPIReclaimMemory: return L"EfiACPIReclaimMemory";
+    case EfiACPIMemoryNVS: return L"EfiACPIMemoryNVS";
+    case EfiMemoryMappedIO: return L"EfiMemoryMappedIO";
+    case EfiMemoryMappedIOPortSpace: return L"EfiMemoryMappedIOPortSpace";
+    case EfiPalCode: return L"EfiPalCode";
+    case EfiPersistentMemory: return L"EfiPersistentMemory";
+    case EfiMaxMemoryType: return L"EfiMaxMemoryType";
+    default: return L"InvalidMemoryType";
+  }
+}
+
+EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
+  if (map->buffer_size == 0) {
+    return EFI_BUFFER_TOO_SMALL;
+  }
+  map->map_size = map->buffer_size;
+  return gBS->GetMemoryMap(
+    &map->map_size,
+    (EFI_MEMORY_DESCRIPTOR*)buffer,
+    &map->discriptor_size,
+    &map->discriptor_version);
+}
+
+EFI_STATUS SaveMemoryMap(struct MemoryMap* map, EFI_FILE_PROTOCOL* file) {
+  CHAR8* header =
+    "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n";
+  UINTN len = AsciiStrLen(header);
+  file->Write(file, &len, header);
+
+  char buffer[256];
+  EFI_PHYSICAL_ADDRESS iter;
+  int i;
+  for (iter = (UINTN)map->buffer, i = 0;
+       iter < map->map_size;
+       iter += map->discriptor_size, i++;) {
+    EFI_MEMORY_DISCRIPTOR* desc = (EFI_MEMORY_DISCRIPTOR*)iter;
+    len = AsciiPrint(
+        buffer, sizeof(buffer),
+        "%u, %x, %-ls, %08lx, %lx, %lx\n",
+        i, desc->type, GetMemoryTypeUnicode(desc->Type),
+        desc->PhysicalStart, desc->NumberOfPages,
+        desc->Attribute & 0xffffflu);
+    file->Write(file, &len, buffer);
+  }
+  return EFI_SUCCESS;
+}
+
+EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL** root) {
+  EFI_LOADED_IMAGE_PROTOCOL* loaded_image;
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* fs;
+
+  gBS->OpenProtocol(
+    image_handle,
+    &gEfiLoadedImageProtocolGuid,
+    (VOID**)&loaded_image,
+    image_handle,
+    NULL,
+    EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+
+  gBS->OpenProtocol(
+    image_handle->DeviceHandle,
+    &gEfiSimpleFileSystemProtocolGuid,
+    (VOID**)&fs,
+    image_handle,
+    NULL,
+    EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+
+  fs->OpenVolume(fs, root);
+  return EFI_SUCCESS;
+}
 
 EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
-    EFI_SYSTEM_TABLE *system_table) {
-  Print(L"Hello, Mikan World!\n");
+    EFI_SYSTEM_TABLE* system_table) {
+  CHAR8 buffer[4096 * 4];
+  struct MemoryMap memmap = {sizeof(buffer), 0, buffer, 0, 0, 0};
+  GetMemoryMap(&memmap);
+
+  EFI_FILE_PROTOCOL* root_dir;
+  OpenRootDir(image_handle, &root_dir);
+  EFI_FILE_PROTOCOL* memmap_file;
+  root_dir->Open(
+    root_dir, &memmap_file, L"\\memmap",
+    EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE  | EFI_FILE_MODE_CREATE, 0);
+
+  SaveMemoryMap(&memmap, memmap_file);
+  memmap_file->Close(memmap_file);
+  Print(L"All done\n");
   while (1);
   return EFI_SUCCESS;
 }
